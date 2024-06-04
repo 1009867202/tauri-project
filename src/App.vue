6322e2b22c347d5331d232891374675a7c1f7665
leviRa3 <levi@wizmacau.com>
2024-06-04 11:34:10 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="name">peng</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.name {
+  width: 200px;
+  background: red;
+  line-height: 30px;
+  text-align: 30px;
+  margin: 0 auto;
+}
+</style>
